2cf9bc42981db071e49dfc5d23662598d70cd82b
ChehAchraf <achrafchehboun@outlook.com>
2025-09-22 09:48:14 +0100

    first commit

---
+++ src/app/Menu.java
@@ -0,0 +1,16 @@
+package app;
+
+import java.util.Scanner;
+
+public class Menu {
+    private Scanner scanner;
+    private boolean isActive;
+
+    public Menu() {
+        this.scanner = new Scanner(System.in);
+        this.isActive = true;
+    }
+
+    public void startApplication
+
+}
