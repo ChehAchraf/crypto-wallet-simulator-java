637c4818483e7d6a3dd041ffbf1c8e40fe50afa1
ChehAchraf <achrafchehboun@outlook.com>
2025-09-22 14:13:53 +0100

    added the static data in bitcoin to calculate fees

--- src/model/Transaction.java
+++ src/model/Transaction.java
@@ -17,6 +17,86 @@ public abstract class Transaction {
     protected CryptoType cryptoType;
     protected FeeCalculationStrategy feeStrategy;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFromAddress() {
+        return fromAddress;
+    }
+
+    public void setFromAddress(String fromAddress) {
+        this.fromAddress = fromAddress;
+    }
+
+    public String getToAddress() {
+        return toAddress;
+    }
+
+    public void setToAddress(String toAddress) {
+        this.toAddress = toAddress;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public double getFees() {
+        return fees;
+    }
+
+    public void setFees(double fees) {
+        this.fees = fees;
+    }
+
+    public FeePriority getFeePriority() {
+        return feePriority;
+    }
+
+    public void setFeePriority(FeePriority feePriority) {
+        this.feePriority = feePriority;
+    }
+
+    public TransactionStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TransactionStatus status) {
+        this.status = status;
+    }
+
+    public CryptoType getCryptoType() {
+        return cryptoType;
+    }
+
+    public void setCryptoType(CryptoType cryptoType) {
+        this.cryptoType = cryptoType;
+    }
+
+    public FeeCalculationStrategy getFeeStrategy() {
+        return feeStrategy;
+    }
+
+    public void setFeeStrategy(FeeCalculationStrategy feeStrategy) {
+        this.feeStrategy = feeStrategy;
+    }
+
     public Transaction(String from, String to, double amount, FeePriority feePriority, FeeCalculationStrategy strategy) {
         this.fromAddress = from;
         this.toAddress = to;
--- src/strategy/BitcoinFeeStrategy.java
+++ src/strategy/BitcoinFeeStrategy.java
@@ -1,4 +1,17 @@
 package strategy;
+import model.Transaction;
+public class BitcoinFeeStrategy implements FeeCalculationStrategy {
+    private static final int AVERAGE_BLOCK_TIME_SEC = 10 * 60;
+    private static final int ESTIMATED_TX_SIZE_BYTES = 250;
+    private static final double SATOSHI_PER_BYTE = 50;
 
-public class BitcoinFeeStrategy {
+    @Override
+    public double calculateFees(Transaction ts) {
+        return ts.getAmount() * 0.001 ;
+    }
+
+    @Override
+    public int estimateConfirmationTime(Transaction tx) {
+        return 0;
+    }
 }
