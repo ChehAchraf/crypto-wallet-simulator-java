55d5b79b26310dd54469a274ca9c87562187a66c
ChehAchraf <achrafchehboun@outlook.com>
2025-09-22 14:31:25 +0100

    added teh static data to calculate fees in the ethereumStrategy

--- src/strategy/EthereumFeeStrategy.java
+++ src/strategy/EthereumFeeStrategy.java
@@ -1,4 +1,27 @@
 package strategy;
+import model.Transaction;
 
-public class EthereumFeeStrategy {
+
+import static enums.FeePriority.*;
+
+public class EthereumFeeStrategy implements FeeCalculationStrategy{
+    private static final int AVERAGE_BLOCK_TIME_SEC = 13;
+    private static final int DEFAULT_GAS_LIMIT = 21000;
+    private static final double DEFAULT_GAS_PRICE_GWEI = 50;
+
+    @Override
+    public double calculateFees(Transaction ts) {
+        double gasPriceEth = DEFAULT_GAS_PRICE_GWEI * 1e-9;
+        return DEFAULT_GAS_LIMIT * gasPriceEth;
+    }
+
+    @Override
+    public int estimateConfirmationTime(Transaction ts) {
+        switch (ts.getFeePriority()) {
+            case ECONOMIQUE: return 5 * AVERAGE_BLOCK_TIME_SEC; // tx lente
+            case STANDARD:   return 2 * AVERAGE_BLOCK_TIME_SEC; // tx normale
+            case RAPIDE:     return 1 * AVERAGE_BLOCK_TIME_SEC; // tx rapide
+            default: return 2 * AVERAGE_BLOCK_TIME_SEC;
+        }
+    }
 }
