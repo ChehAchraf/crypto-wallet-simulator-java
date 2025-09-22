a002d076c2e28b7b488e78b3ab99b9427a56884e
ChehAchraf <achrafchehboun@outlook.com>
2025-09-22 14:24:39 +0100

    overrided the estimate confirmation time in bitcoin fee strategy

--- src/enums/FeePriority.java
+++ src/enums/FeePriority.java
@@ -1,7 +1,7 @@
 package enums;
 
 public enum FeePriority {
-    FIRST,
-    AVERAGE,
-    LAST
+    ECONOMIQUE,
+    STANDARD,
+    RAPIDE
 }
--- src/strategy/BitcoinFeeStrategy.java
+++ src/strategy/BitcoinFeeStrategy.java
@@ -1,5 +1,6 @@
 package strategy;
 import model.Transaction;
+import enums.*;
 public class BitcoinFeeStrategy implements FeeCalculationStrategy {
     private static final int AVERAGE_BLOCK_TIME_SEC = 10 * 60;
     private static final int ESTIMATED_TX_SIZE_BYTES = 250;
@@ -7,11 +8,16 @@ public class BitcoinFeeStrategy implements FeeCalculationStrategy {
 
     @Override
     public double calculateFees(Transaction ts) {
-        return ts.getAmount() * 0.001 ;
+        return ESTIMATED_TX_SIZE_BYTES *  SATOSHI_PER_BYTE * 1e-8 ;
     }
 
     @Override
     public int estimateConfirmationTime(Transaction tx) {
-        return 0;
+        switch(tx.getFeePriority()){
+            case ECONOMIQUE: return 3 * AVERAGE_BLOCK_TIME_SEC;
+            case STANDARD:   return 2 * AVERAGE_BLOCK_TIME_SEC;
+            case RAPIDE:     return 1 * AVERAGE_BLOCK_TIME_SEC;
+            default: return 2 * AVERAGE_BLOCK_TIME_SEC;
+        }
     }
 }
